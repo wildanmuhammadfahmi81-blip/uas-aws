5642945a76e3aa1bd31cbe126088bfa655125fc9
wildanmuhammadfahmi81 <wildanmuhammadfahmi81@gmail.com>
2026-06-08 20:20:32 +0700

    update index.php

--- dynamic-app/public/index.php
+++ dynamic-app/public/index.php
@@ -81,7 +81,7 @@ $notes = db()->query(
     <main class="shell">
         <header class="topbar">
             <div>
-                <p class="eyebrow">UAS Administrasi Server Wildan M Fahmi_2388010030</p>
+                <p class="eyebrow">UAS Administrasi Server</p>
                 <h1><?= e(APP_NAME) ?></h1>
             </div>
             <div class="account">
